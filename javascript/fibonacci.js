731e97bb1f05454363cca9851c09de14b6431058
Daniel Calvin <arockne2003@yahoo.com>
2022-02-17 09:21:55 -0700

    commit tree feec7f07a6950e07860c9ed374f9a28ccd309140

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -21,4 +21,55 @@ if (require.main === module) {
 module.exports = fibonacci;
 
 // Please add your pseudocode to this file
+
+
+//i 
+  //nth index
+//o
+  //fib number 
+//e
+  //none
+//c
+  //none
+//HLP
+  /*
+    Find the nth element in fibonacci series
+    fibbonacci sequence start with a 0 then a 1
+    after that every value is the sum of the two values preceding it
+    example: 
+      input = 10
+      0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+  */
+  function fibonacci(n) {
+    if (n === 0) {
+      return 0
+    }
+    if (n === 1) {
+      return 1
+    }
+
+    const fibArray = [0, 1];
+    
+    for (let i = 2; i < n; i++) {
+      fibArray.push(fibArray[i - 1] + fibArray[i - 2])
+    }
+
+    return fibArray[fibArray.length - 1] + fibArray[fibArray.length - 2];
+  }
+
+  function fibonacci(n) {
+    let nth = 0;
+    let first = 0;
+    let second = 1;
+
+    for (let i = 2; i <= n; i++) {
+      nth = first + second
+      first = second
+      second = nth
+    }
+    return nth;
+  }
+
 // And a written explanation of your solution
+
+
